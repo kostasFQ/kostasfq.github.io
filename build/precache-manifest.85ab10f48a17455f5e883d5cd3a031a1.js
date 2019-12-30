ab08358a984de1e5be9e31561925602ae198a605
Kanstantsin Asipenka <kanstantsin.asipenka@1pt.com>
2019-12-30 16:56:06 +0300

    build

---
+++ build/precache-manifest.85ab10f48a17455f5e883d5cd3a031a1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbd6be89860bdbeed47b76da2269d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "625f0a218ca091044df4",
+    "url": "/static/css/main.11255c62.chunk.css"
+  },
+  {
+    "revision": "7aea0a2cb06b404585dc",
+    "url": "/static/js/2.80186afc.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/static/js/2.80186afc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "625f0a218ca091044df4",
+    "url": "/static/js/main.e9077c5b.chunk.js"
+  },
+  {
+    "revision": "b1aed7cfe51f6924684e",
+    "url": "/static/js/runtime-main.82f5791a.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "bc6bf3ed87a1d7084b8f5d39e7c23646",
+    "url": "/static/media/email.bc6bf3ed.svg"
+  },
+  {
+    "revision": "42c9ead6ddefbf42741beb6f680c34b3",
+    "url": "/static/media/github.42c9ead6.svg"
+  },
+  {
+    "revision": "d011faf2fc88598075f76091cca58c9c",
+    "url": "/static/media/home.d011faf2.svg"
+  },
+  {
+    "revision": "f3edbace08c0353c4af580efa8917486",
+    "url": "/static/media/linked.f3edbace.svg"
+  },
+  {
+    "revision": "9a8c3f401a4508b1171036e2d6fe1915",
+    "url": "/static/media/phone.9a8c3f40.svg"
+  },
+  {
+    "revision": "f785b3ee2946f2d10c777ea3b31288a8",
+    "url": "/static/media/skype.f785b3ee.svg"
+  }
+]);
