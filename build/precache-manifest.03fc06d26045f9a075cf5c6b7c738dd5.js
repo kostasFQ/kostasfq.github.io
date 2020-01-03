097d9521ac094883ba558aa762079ace791a8729
Kanstantsin Asipenka <kanstantsin.asipenka@1pt.com>
2020-01-03 14:04:54 +0300

    depl

---
+++ build/precache-manifest.03fc06d26045f9a075cf5c6b7c738dd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7b00ffbcf23aeae2f1b9a362108479",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "54516ed90712a811669f",
+    "url": "/resume/static/css/main.0d341e77.chunk.css"
+  },
+  {
+    "revision": "9504ca74f2dd970f3b3a",
+    "url": "/resume/static/js/2.a690add1.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/resume/static/js/2.a690add1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54516ed90712a811669f",
+    "url": "/resume/static/js/main.84a85604.chunk.js"
+  },
+  {
+    "revision": "52eff69184e16e5747c6",
+    "url": "/resume/static/js/runtime-main.b734fd6f.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/resume/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  }
+]);
