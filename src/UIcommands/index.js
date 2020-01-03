8726366a133f7f245bff938205e2a55edb828b34
Kanstantsin Asipenka <kanstantsin.asipenka@1pt.com>
2020-01-03 14:12:32 +0300

    socials

--- src/UIcommands/index.js
+++ src/UIcommands/index.js
@@ -1,7 +1,7 @@
 import { socials } from 'assets/resume.json';
 
-const toGitHub = () => { window.open(socials.github) };
-const toLinked = () => { window.open(socials.linkedin) };
+const toGitHub = () => { window.open(socials.gitHub) };
+const toLinked = () => { window.open(socials.linkedIn) };
 const print = () => { window.print() };
 const email = () => { window.location.href = "mailto:kostasfq@gmail.com"; };
 const telegram = () => { window.open('https://web.telegram.org//#/im?p=@kostasfq') };
